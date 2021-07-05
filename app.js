734c5bbafd4eefd0046427cc3ffcf5b4f588f1ca
Bhupesh Suryavanshi <bhupesh6290@gmail.com>
2021-07-06 00:24:18 +0530

    minor changes

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ p1Btn.addEventListener('click', function() {
         isGameOver = true;
         p1Display.classList.add('has-text-success');
         p2Display.classList.add('has-text-danger');
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
     }
     p1Display.textContent = p1Score;
 }
@@ -29,6 +31,8 @@ p2Btn.addEventListener('click', function() {
         isGameOver = true;
         p2Display.classList.add('has-text-success');
         p1Display.classList.add('has-text-danger');
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
     }
     p2Display.textContent = p2Score;
 }
@@ -49,4 +53,6 @@ function reset() {
     p2Display.textContent = 0;
     p1Display.classList.remove('has-text-success', 'has-text-danger');
     p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
 }
